d0c4be4fcfcf9e946228c05b9a32108b2fa1c654
enchanted-coder <danieltogey@pm.me>
2023-04-13 15:43:42 +0300

    added bootstrap list using list-group and list-group-item

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import Message from './Message'
+import ListGroup from './components/ListGroup'
 
 const App = () => {
-  return <Message />
+  //return <Message />
+  return <ListGroup />
 }
 
 export default App
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,13 @@
+const ListGroup = () => {
+    return(
+        <ul className="list-group">
+            <li className="list-group-item">Mangoes</li>
+            <li className="list-group-item">Apples</li>
+            <li className="list-group-item">Oranges</li>
+            <li className="list-group-item">Watermelon</li>
+            <li className="list-group-item">Papaya</li>
+        </ul>
+    )
+}
+
+export default ListGroup
